59cf6c4294716a2519482b370672c9deeb5e2021
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-02 18:55:56 +0300

    add bootstrap and form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DiaryFormComponent } from './diary-form/diary-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DiaryFormComponent
   ],
   imports: [
     BrowserModule,
