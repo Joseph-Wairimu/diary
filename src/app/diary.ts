7fa0586a02bc65b5982cfab56c3a613b7389edb5
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-04 08:21:08 +0300

    initialise id and name in class blueprint and add a constructor

--- src/app/diary.ts
+++ src/app/diary.ts
@@ -1,4 +1,12 @@
 export class Diary {
-
     
-}
+    id: number;
+    name: string;
+    // description: string;
+    constructor(id:number,name: string,description: string){
+        this.id= id;
+        this.name=name;
+        // this.description=description;
+    }
+
+  }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Diary } from '../diary';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  diarys:Diary[] = [
+    {id:1, name:'Watch finding Nemo'},
+    {id:2,name:'Buy Cookies'},
+    {id:3,name:'Get new Phone Case'},
+    {id:4,name:'Get Dog Food'},
+    {id:5,name:'Solve math homework'},
+    {id:6,name:'Plot my world domination plan'},
+  ];
 
   constructor() { }
 
