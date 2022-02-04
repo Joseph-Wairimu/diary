876a3813048ca634341d2558a683892e65d3e06d
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-04 09:21:59 +0300

    use custom date pipe

--- src/app/diary.ts
+++ src/app/diary.ts
@@ -2,7 +2,7 @@ export class Diary {
     
    
     showDescription: boolean;
-    constructor(public id: number,public name: string,public description: string){
+    constructor(public id: number,public name: string,public description: string, public completeDate: Date){
       this.showDescription=false;
     }
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,12 +7,12 @@ import { Diary } from '../diary';
 })
 export class HomeComponent implements OnInit {
   diarys:Diary[] =[
-    new Diary(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
-    new Diary(2,'Buy Cookies','I have to buy cookies for the parrot'),
-    new Diary(3,'Get new Phone Case','Diana has her birthday coming up soon'),
-    new Diary(4,'Get Dog Food','Pupper likes expensive snacks'),
-    new Diary(5,'Solve math homework','Damn Math'),
-    new Diary(6,'Plot my world domination plan','Cause I am an evil overlord'),
+    new Diary(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
+    new Diary(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
+    new Diary(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
+    new Diary(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
+    new Diary(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
+    new Diary(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
   ];
 
 Hide(index:any){
